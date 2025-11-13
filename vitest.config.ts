8fadcec824eeda82b50e707ecf5f33eca809e64b
Kevin Hill <kevinkhill@pm.me>
2025-11-12 17:58:56 -0800

    upping timeout to 10s

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vitest/config";
 
+const TEN_SECONDS = 10 * 1000;
+
 export default defineConfig({
 	test: {
 		maxWorkers: 1,
+		testTimeout: TEN_SECONDS,
 		setupFiles: ["./test/_setup/expect.ts"],
 		coverage: {
 			enabled: false,
